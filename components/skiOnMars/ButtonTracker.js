bcbf23104a85bf03480861d194ad21f8609c4dfb
Salem Dahmani <salem.dahmani345@gmail.com>
2022-10-25 01:23:22 +0200

    BIG BOY

--- components/skiOnMars/ButtonTracker.js
+++ components/skiOnMars/ButtonTracker.js
@@ -1,57 +1,126 @@
-import { View, Text, ScrollView, StyleSheet} from 'react-native'
-import { TouchableOpacity } from 'react-native-gesture-handler'
-import React from 'react'
-import Button from '../profile/Button'
-import { useNavigation } from '@react-navigation/native'
-import SingleActivity from './SingleActivity'
+import { View, Text , StyleSheet, TouchableOpacity  , ScrollView} from 'react-native'
+import React, { useContext, useEffect, useState } from 'react'
+import {Avatar } from 'react-native-paper';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { AuthContext } from '../../utils/auth-context';
+import app from '../../utils/config';
+import { getDatabase, ref, onValue, set  , get , child , update} from 'firebase/database';
+import { firebase } from '@react-native-firebase/auth';
+import SingleActivity from './SingleActivity';
+import { useFocusEffect } from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
+
+const ListActivies = () => {
+
+  const navigation = useNavigation()
+
+
+  var dataT = [];
+
+  const  [data , setData] = React.useState([])
+
+  const Auth = useContext(AuthContext)
+  const uid = firebase.auth().currentUser.uid
 
 
 
 
-const ButtonTracker = () => {
+
+ 
+useFocusEffect(
+
+  React.useCallback(() => {
+    setData([])
+    const db = getDatabase(app);
+    const reference = ref(db, 'activities/' + uid);
+    onValue(reference, (snapshot) => {
+      for (a in snapshot.val()){
+      //  console.log(a)
+      setData(data => [...data,snapshot.val()[a]] );
+   //  console.log(data)
+     
+      }
+     
+    });
+   
+  
 
   
 
+
+   
+  }, [])
+);
+
+
+
+
   return (
-<View style={{flex: 1 }}>
+<View style={{flex : 1 }}>
+  <View style={styles.container}>
+ 
+     <View>
+            
+          </View>
+          <View style={{flex: 1, bottom : '7%'  , alignItems : 'center'}}>
     <ScrollView
     keyboardShouldPersistTaps='always'
-    contentContainerStyle={{ alignItems : 'center' , paddingVertical : 30}}>
+    contentContainerStyle={{ alignItems : 'center'  }}>
        
   
 
-
-        <TouchableOpacity style={style.container} >
-        <SingleActivity record={2}/>
+ 
+ {data.slice(0, 2).map((element ,i) => {
+       return (
+        <TouchableOpacity key={i} style={styles.container} >
+  
+        <SingleActivity data={element}/>
         </TouchableOpacity>
+      ) 
+      })} 
 
 
 
-        <TouchableOpacity style={style.container} >
-        <SingleActivity/>
-        </TouchableOpacity>
-
-
-
-
-        <TouchableOpacity style={style.container} >
-        <SingleActivity/>
-        </TouchableOpacity>
-
-
+<View ></View>
          
 
     </ScrollView>
+    <TouchableOpacity onPress={()=> navigation.navigate('listA')} >
+                    <View style={{ top : 20 ,  width: 113,height: 40,borderRadius: 15,borderColor: '#ffd6c7',borderStyle: 'solid',justifyContent :'center',borderWidth: 1,backgroundColor: '#ffffff'}}>
+                        <Text style={{ color: '#666666',fontFamily: 'Museo',fontSize: 14,fontWeight: '400',fontStyle: 'normal',textAlign: 'center',lineHeight: 22,}}>View all</Text>
+                    </View>
+                </TouchableOpacity>
+    
   </View>
+
+    </View>
+
+</View>
   )
 }
 
-export default ButtonTracker
+export default ListActivies
 
 
-const style = StyleSheet.create({
-  container: {
-       marginVertical : 20,
-  },
 
-});
+const styles = StyleSheet.create({
+    container :{
+     flex : 1 ,
+     backgroundColor :'white',
+     padding : 20
+     
+    },
+    notification: {
+      color: '#000000',
+      fontFamily: 'Esoris',
+      fontSize: 28,
+      fontWeight: '400',
+      fontStyle: 'normal',
+      lineHeight: 36,
+      paddingHorizontal : 20,
+      marginTop : 10,
+    },
+})
+
+
+
